d75455a390f263acb079c2a0c954e67012e2f480
Sagor Roy <sagorroy204@gmaile.com>
2024-01-08 17:45:43 +0600

    update

--- src/backend/partials/Sidebar.jsx
+++ src/backend/partials/Sidebar.jsx
@@ -1,42 +1,51 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 function Sidebar() {
+    const location = useLocation();
     const [tree, setTree] = useState([
         {
             title: "Dashboard",
             url: "/admin",
+            status: false,
             children: []
         },
         {
             title: "Ticket Management",
             url: "#",
+            status: false,
             children: [
                 {
                     title: "Booking Information",
                     url: "#",
+                    status: false,
                     children: [
                         {
                             title: "Add Booking",
-                            url: "#"
+                            url: "#",
+                            status: false,
                         },
                         {
                             title: "Booking List",
-                            url: "#"
+                            url: "#",
+                            status: false,
                         }
                     ]
                 },
                 {
                     title: "Passenger Information",
                     url: "#",
+                    status: false,
                     children: [
                         {
                             title: "Add Passenger",
-                            url: "#"
+                            url: "#",
+                            status: false,
                         },
                         {
                             title: "Passenger List",
-                            url: "#"
+                            url: "#",
+                            status: false,
                         }
                     ]
                 }
@@ -45,32 +54,39 @@ function Sidebar() {
         {
             title: "Fleet Management",
             url: "#",
+            status: false,
             children: [
                 {
                     title: "Fleet Type",
                     url: "#",
+                    status: false,
                     children: [
                         {
                             title: "Add Fleet",
-                            url: "#"
+                            url: "#",
+                            status: false,
                         },
                         {
                             title: "Fleet List",
-                            url: "#"
+                            url: "#",
+                            status: false,
                         }
                     ]
                 },
                 {
                     title: "Vehicles",
                     url: "#",
+                    status: false,
                     children: [
                         {
                             title: "Add Vehicles",
-                            url: "#"
+                            url: "#",
+                            status: false,
                         },
                         {
                             title: "Vehicles List",
-                            url: "#"
+                            url: "#",
+                            status: false,
                         }
                     ]
                 }
@@ -79,66 +95,108 @@ function Sidebar() {
         {
             title: "Trip Management",
             url: "#",
+            status: false,
             children: [
                 {
                     title: "Destination",
                     url: "#",
+                    status: false,
                     children: [
                         {
                             title: "Add Destination",
-                            url: "destination/create"
+                            url: "destination/create",
+                            status: false,
                         },
                         {
                             title: "Destination List",
-                            url: "destination/list"
+                            url: "destination/list",
+                            status: false,
                         }
                     ]
                 },
                 {
                     title: "Route",
                     url: "#",
+                    status: false,
                     children: [
                         {
                             title: "Add Route",
-                            url: "route/create"
+                            url: "route/create",
+                            status: false,
                         },
                         {
                             title: "Route List",
-                            url: "route/list"
+                            url: "route/list",
+                            status: false,
                         }
                     ]
                 },
                 {
                     title: "Schedule",
                     url: "#",
+                    status: false,
                     children: [
                         {
                             title: "Add Schedule",
-                            url: "schedule/create"
+                            url: "schedule/create",
+                            status: false,
                         },
                         {
                             title: "Schedule List",
-                            url: "schedule/list"
+                            url: "schedule/list",
+                            status: false,
                         }
                     ]
                 },
                 {
                     title: "Trip",
                     url: "#",
+                    status: false,
                     children: [
                         {
                             title: "Add Trip",
-                            url: "#"
+                            url: "#",
+                            status: false,
                         },
                         {
                             title: "Trip List",
-                            url: "#"
+                            url: "#",
+                            status: false,
                         }
                     ]
                 }
             ],
         }
     ]);
+
+    const cleanPath = (path) => {
+        const withoutLeadingSlash = path.startsWith('/') ? path.substring(1) : path;
+        return withoutLeadingSlash.startsWith('admin/') ? withoutLeadingSlash.substring(6) : withoutLeadingSlash;
+    };
+
+    console.log(cleanPath(location.pathname));
+
+    const updateStatusRecursively = (items, currentPath) => {
+        return items.map((item) => {
+            const updatedItem = { ...item };
+            const cleanedPath = cleanPath(currentPath);
+            if (item.url === cleanedPath) {
+                updatedItem.status = true;
+            } else if (item.children && item.children.length > 0) {
+                updatedItem.children = updateStatusRecursively(item.children, cleanedPath);
+                updatedItem.status = updatedItem.children.some((child) => child.status);
+            } else {
+                updatedItem.status = false;
+            }
+            return updatedItem;
+        });
+    };
+
+    useEffect(() => {
+        setTree((prevTree) => updateStatusRecursively(prevTree, location.pathname));
+    }, [location.pathname]);
+
+    console.log(tree);
 
     const [openStates, setOpenStates] = useState(tree.map(() => false));
     const [childOpenStates, setChildOpenStates] = useState(tree.map(() => []));
@@ -151,18 +209,20 @@ function Sidebar() {
             newStates[index] = !newStates[index];
             return newStates;
         });
+        childToggleTree(index, null)
     };
 
     const childToggleTree = (parentIndex, childIndex) => {
         setChildOpenStates((prev) => {
             const newStates = [...prev];
+            newStates[parentIndex] = [];
             newStates[parentIndex][childIndex] = !newStates[parentIndex][childIndex];
             return newStates;
         });
     };
 
     const renderTree = (item, parentIndex) => (
-        <li className={`treeview ${openStates[parentIndex] ? 'menu-open' : ''}`} key={parentIndex}>
+        <li className={`treeview ${item?.status ? 'menu-open active' : ''} ${openStates[parentIndex] ? 'menu-open' : ''}`} key={parentIndex}>
             <Link to={item?.url} onClick={() => toggleTree(parentIndex)}>
                 <i className="fa fa-circle-o text-red"></i> <span>{item.title}</span>
                 {item?.children?.length > 0 && (
@@ -172,7 +232,7 @@ function Sidebar() {
                 )}
             </Link>
             {item?.children?.length > 0 && (
-                <ul className="treeview-menu" style={{ display: openStates[parentIndex] ? 'block' : 'none', paddingLeft: '20px' }}>
+                <ul className="treeview-menu" style={{ display: openStates[parentIndex] || item?.status ? 'block' : 'none', paddingLeft: '20px' }}>
                     {item.children.map((childItem, childIndex) => (
                         childRenderTree(childItem, parentIndex, childIndex)
                     ))}
@@ -182,7 +242,7 @@ function Sidebar() {
     );
 
     const childRenderTree = (item, parentIndex, childIndex) => (
-        <li className={`treeview ${childOpenStates[parentIndex][childIndex] ? 'menu-open' : ''}`} key={childIndex}>
+        <li className={`treeview ${item?.status ? 'menu-open active' : ''} ${childOpenStates[parentIndex][childIndex] ? 'menu-open' : ''}`} key={childIndex}>
             <Link to={item?.url} onClick={() => childToggleTree(parentIndex, childIndex)}>
                 <i className="fa fa-circle-o"></i> <span>{item.title}</span>
                 {item?.children?.length > 0 && (
@@ -192,9 +252,9 @@ function Sidebar() {
                 )}
             </Link>
             {item?.children?.length > 0 && (
-                <ul className="treeview-menu" style={{ display: childOpenStates[parentIndex][childIndex] ? 'block' : 'none' }}>
+                <ul className="treeview-menu" style={{ display: childOpenStates[parentIndex][childIndex] || item?.status ? 'block' : 'none' }}>
                     {item.children.map((subChildItem, subChildIndex) => (
-                        <li className={`treeview `} key={subChildIndex}>
+                        <li className={`treeview ${subChildItem?.status ? 'menu-open active' : ''}`} key={subChildIndex}>
                             <Link to={subChildItem?.url}>
                                 <i className="fa fa-circle-o"></i> <span>{subChildItem.title}</span>
                             </Link>
